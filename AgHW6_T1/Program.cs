a881d9877647cee6ed6bb1f54b5fdc22278376cb
Андрей Сергеевич Агальцов <a.s.agaltsov@yandex.ru>
2022-11-02 17:11:23 +0300

    Добавьте файлы проекта.

---
+++ AgHW6_T1/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AgHW6_T1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string sentence = GetSentence();
+            int numberChar = GetNumberChar();
+            char[] charToFind = new char[numberChar];
+            PrintResult(sentence, charToFind);
+            Console.ReadKey();
+        }
+        static string GetSentence()
+        {
+            string userString = string.Empty;
+            //input string
+            return userString;
+        }
+        static int GetNumberChar()
+        {
+            int numberChar = 0;
+            //input int
+            return numberChar;
+        }
+        static char[] GetCharsToFind(int arrayLength)
+        {
+            char[] charsToFind = new char[arrayLength];
+            for (int i = 0; i < charsToFind.Length; i++)
+            {
+                //input chars
+            }
+            return charsToFind;
+        }
+        static void PrintResult(string sentense, char[] charToFind)
+        {
+            //find
+            //print
+        }
+    }
+}
