00366bb01da02aceecc74a7ad69be0453e8b7506
Андрей Сергеевич Агальцов <a.s.agaltsov@yandex.ru>
2022-11-03 18:58:54 +0300

    2

--- AgHW6_T1/Program.cs
+++ AgHW6_T1/Program.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace AgHW6_T1
 {
@@ -12,7 +8,7 @@ namespace AgHW6_T1
         {
             string sentence = GetSentence();
             int numberChar = GetNumberChar();
-            char[] charToFind = new char[numberChar];
+            char[] charToFind = GetCharsToFind(numberChar);
             PrintResult(sentence, charToFind);
             Console.ReadKey();
         }
@@ -24,25 +20,43 @@ namespace AgHW6_T1
             Console.WriteLine("Введите предложение:");
             do
             {
-                cki = Console.ReadKey();
-                if (IsLegacyChar(cki.KeyChar))
-                userString += cki.KeyChar.ToString();   
+                cki = Console.ReadKey(true);
+                userChar = cki.KeyChar;
+                if (IsLegacyChar(userChar))
+                {
+                    userString += userChar;
+                    Console.Write(userChar);
+                }
+                if (cki.Key == ConsoleKey.Backspace && userString.Length != 0)
+                {
+                    userString = userString.Remove(userString.Length - 1);
+                    Console.Write("\b \b");
+                }
             }
             while (cki.Key != ConsoleKey.Enter);
+            Console.WriteLine();
             return userString;
         }
         static int GetNumberChar()
         {
-            int numberChar = 0;
-            //input int
+            int numberChar = GetInt32("Введите количество символов для поиска", minValue: 1, maxValue: 100);
             return numberChar;
         }
         static char[] GetCharsToFind(int arrayLength)
         {
+            char userChar;
             char[] charsToFind = new char[arrayLength];
+            Console.WriteLine($"Введите {arrayLength} символов для поиска");
             for (int i = 0; i < charsToFind.Length; i++)
             {
-                //input chars
+                userChar = Console.ReadKey(true).KeyChar;
+                if (!IsLegacyChar(userChar))
+                    i--;
+                else
+                {
+                    Console.Write(userChar);
+                    charsToFind[i] = userChar; 
+                }
             }
             return charsToFind;
         }
@@ -53,8 +67,24 @@ namespace AgHW6_T1
         }
         static bool IsLegacyChar(char ch)
         {
-            //IsLegacy?
-            return true;
+            if (Char.IsLetter(ch) || Char.IsPunctuation(ch) || Char.IsWhiteSpace(ch))
+                return true;
+            return false;
+        }
+        static int GetInt32(string prompt = "", int minValue = Int32.MinValue, int maxValue = Int32.MaxValue)
+        {
+            string inputStr = string.Empty;
+            while (true)
+            {
+                Console.WriteLine($"Введите {prompt}");
+                Console.Write($"(Целое число от {minValue} до {maxValue}): ");
+                inputStr = Console.ReadLine();
+                if (int.TryParse(inputStr, out int value))
+                    if (value >= minValue && value <= maxValue)
+                    {
+                        return value;
+                    }
+            }
         }
     }
 }
